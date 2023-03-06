f10fb2659c256601c18fe0453a89099337daa762
qkrdpwls1223 <yejinn_0819@naver.com>
2023-03-06 22:54:09 +0900

    access token발급 코드, DB변경

--- app/views/main_views.py
+++ app/views/main_views.py
@@ -3,10 +3,10 @@ from flask import Blueprint
 from flask import current_app
 
 # MySQl 연결 관련
-from sqlalchemy import text, MetaData, update, exc
+from sqlalchemy import text
 
 # 클라이언트 통신 관련
-from flask import render_template, request, redirect, url_for, session
+from flask import render_template, request, redirect, url_for
 
 # 소켓 통신
 from flask_socketio import emit
@@ -24,28 +24,43 @@ from datetime import datetime, timedelta
 # Token생성 JWT라이브러리
 import jwt
 
+# json형식 http응답
+from flask import jsonify
+
+# SHA256
+import hashlib
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
 svrURL = 'http://203.232.193.164:5000/'
 #svrURL = 'http://localhost:5000/'
 
-def generate_access_token(uuid):
+def generate_access_token(user_id):
+    stmt = "SELECT uuid FROM users WHERE ='" + user_id +"'"
+    with current_app.database.connect() as conn:
+        for row in conn.execute(text(stmt)):
+            uuid = row.uuid
+
+    iat = datetime.utcnow()
+    exp = datetime.utcnow() + timedelta(days=1)
     payload = {
         'sub': uuid,
-        'iat': datetime.utcnow(),
-        'exp': datetime.utcnow() + timedelta(days=1)
+        'iat': iat,
+        'exp': exp
     }
-    access_token = jwt.encode(payload, '221124104', algorithm='HS256')
-    return access_token.decode('utf-8')
+    access_token = jwt.encode(payload, '221124104', algorithm='HS256').decode('utf-8')
+
+    # DB에 access token 저장
+    with current_app.database.connect() as conn:
+        stmt = f"""REPLACE INTO access_tokens(user_id, access_token, access_token_issued_at, aceess_token_expiration_time)
+                VALUES('{user_id}', '{access_token}', '{iat}', '{exp}')"""
+        conn.execute(text(stmt))
+
+    return access_token
 
 @bp.route("/socket")
 def socket_test():
     return render_template('socketTest.html')
-
-@bp.route("/token")
-def create_token():
-    return secrets.token_hex(16)
 
 @bp.route('/hello')
 def hello_pybo():
@@ -53,44 +68,62 @@ def hello_pybo():
 
 @bp.route('/')
 def index():
-    if 'email' in session:
-        return session['email'] + '님 환영합니다.'
     return 'Pybo index'
 
 @bp.route("/sign-up-page")
 def sign_up_page():
     return render_template('sign-up.html')
 
-@bp.route("/sign-up", methods=['POST'])
-def sign_up():
+# 이메일 중복검사 함수
+@bp.route("/sign-up/check-email-duplication", methods=['POST'])
+def check_email_duplication():
     _email = request.json['email']
-    _password = request.json['password']
-
     # email 검색
     stmt = "SELECT email FROM users WHERE email='" + _email +"'"
     with current_app.database.connect() as conn:
         result = conn.execute(text(stmt)).all()
     if len(result) > 0:
-        return '이미 사용중인 이메일입니다.'
+        return jsonify({'msg': '중복된 이메일', 'result': 0})
+    else:
+        return jsonify({'msg': '사용가능한 이메일', 'result': 1})
 
-    # 이메일, 비밀번호 DB 입력
-    stmt = f"INSERT INTO users(email, password) VALUES('{_email}', '{_password}')" 
-    with current_app.database.connect() as conn:
-        result = conn.execute(text(stmt))
-        conn.commit()
+@bp.route("/sign-up", methods=['POST'])
+def sign_up():
+    _email = request.json['email']
+    _password = request.json['password']
 
-    # uuid 값 가져오기
-    stmt = "SELECT uuid FROM users WHERE email='" + _email +"'"
+    # 솔트값 생성
+    salt = secrets.token_hex(32)
+    _password += salt
+    # 비번 + 솔트 해시값 얻기
+    hash_pw = hashlib.sha256(_password.encode()).hexdigest()
+
+    # users 테이블에 insert
     with current_app.database.connect() as conn:
-        for row in conn.execute(text(stmt)):
-            _uuid = row.uuid
-    
+        stmt = f"INSERT INTO users(email, uuid) VALUES('{_email}', REPLACE(UUID(), '-', ''))" 
+        conn.execute(text(stmt))
+        user_id = conn.lastrowid
+
+    # password 저장
+    with current_app.database.connect() as conn:
+        stmt = f"""INSERT INTO password(user_id, hash_password, salt, update_date)
+                    VALUES('{user_id}', '{hash_pw}', '{salt}', NOW())"""
+        conn.execute(text(stmt))
+
+    # 인증 메일 전송
+    email_verification(user_id, _email)
+    return jsonify({'msg': '가입완료'})
+
+def email_verification(user_id, email):
     # 인증 토큰 생성 후 DB 입력
-    _token = secrets.token_hex(16)
-    stmt = f"INSERT INTO signup_auths(uuid, authToken) VALUES('{_uuid}', '{_token}')"
+    _token = secrets.token_hex(32)      # 결과값 자리수 nbyte*x
+    iat = datetime.utcnow()                         # 발급 시간
+    exp = datetime.utcnow() + timedelta(minutes=5)  # 만료 시간
+
     with current_app.database.connect() as conn:
-        result = conn.execute(text(stmt))
-        conn.commit()
+        stmt = f"""INSERT INTO email_verification_tokens(user_id, token, token_issued_at, token_expiration_time)
+                VALUES('{user_id}', '{_token}', '{iat}', '{exp}')"""
+        conn.execute(text(stmt))
 
     # 인증 메일 발송
     smtp = smtplib.SMTP('smtp.gmail.com', 587)
@@ -100,43 +133,40 @@ def sign_up():
     smtp.login('yjnnn0011@gmail.com', 'riccnlfbvaxrgfmd')
     # 메일 작성
     msg = EmailMessage()
-    msg.set_content(svrURL + f'mailauth?email={_email}&authToken={_token}')
+    msg.set_content(svrURL + f'mailauth?authToken={_token}')
     msg['Subject'] = '[MyWalkingPet] 회원가입 이메일 인증'
     msg['From'] = 'yjnnn0011@gmail.com'
-    msg['To'] = _email
+    msg['To'] = email
 
+    # 메일 전송
     smtp.send_message(msg)
+    # 이메일 연결 종료
     smtp.quit()
-
-    return 'sign-up'
 
 @bp.route('/mailauth', methods=['GET'])
 def mailauth():
-    _email = request.args.get('email')
     _authToken = request.args.get('authToken')
 
-    stmt = "SELECT uuid FROM users WHERE email='" + _email +"'"
     with current_app.database.connect() as conn:
-        for row in conn.execute(text(stmt)):
-            _uuid = row.uuid
+        stmt = f"SELECT * FROM email_verification WHERE token='{_authToken}'"
+        # .fetchone(): 검색결과가 None인지 구분 해주는 메소드
+        row = conn.execute(text(stmt)).fetchone()
+        if row:
+            # 검색 결과가 있을 때 처리
+            user_id = row.user_id
+            iat = row.token_issued_at
+            exp = datetime.strftime(row.token_expiration_time, '%Y-%m-%d %H:%M:%S')
 
-    # 토큰 검색
-    stmt = f"SELECT authToken, issueTime FROM signup_auths WHERE uuid='{_uuid}'"
-    with current_app.database.connect() as conn:
-        for row in conn.execute(text(stmt)):
-            authToken = row.authToken
-            issueTime = row.issueTime
-
-    how_long = datetime.now() - issueTime
-    if _authToken == authToken:
-       if how_long.seconds // 60 <= 5:     
-            stmt = f"UPDATE users SET authSatus = 1 WHERE uuid='{_uuid}'"
-            with current_app.database.connect() as conn:
-                result = conn.execute(text(stmt))
-                conn.commit()
-       else:
-           return '토큰이 만료되었습니다.'
-    return '이메일 인증 완료. 회원가입 되었습니다.'
+            if exp < datetime.now():
+                # 만료 시간보다 현재 시간이 빠를 때
+                stmt = f"UPDATE users SET authSatus = 1 WHERE user_id='{user_id}'"
+                conn.execute(text(stmt))
+                return jsonify({'msg': '이메일 인증 완료'})
+            else:
+                return jsonify({'msg': '만료된 토큰입니다.'})
+        else:
+            return jsonify({'msg': '토큰이 유효하지 않습니다.'})
+            
         
 @bp.route('/login-page')
 def login_info():
@@ -146,16 +176,15 @@ def login_info():
 def login():
     _email = request.form['email']
     _password = request.form['password']
-    if (_email == 'admin' and _password == 'admin'):
-        return 'login!'
-    else:
-        if 'email' in session:
-            print(_email + "님은 이미 로그인 중입니다.")
-            return '이미 로그인 중입니다.'
-        else:
-            print(_email + "님이 로그인하였습니다.")
-            session['email'] = _email
-            return '로그인 성공'
+
+    # email로 user_id, uuid 검색 쿼리
+    # user_id로 hash_pw, salt 검색 쿼리
+    # _password + salt를 해싱 후 hash_password와 비교
+    # 맞으면 password 테이블의 update_date 컬럼 update
+
+    print(_email + "님이 로그인하였습니다.")
+    #access_token = generate_access_token(uuid)
+    #return jsonify({'access_token': access_token})
 
 @bp.route('/mypage')
 def login():
--- main.py
+++ main.py
@@ -17,24 +17,7 @@ from app.socket_views import socket_bp, socketio
 #socketio.on_namespace(socket_bp)
 app.register_blueprint(socket_bp)
 
-oauth = OAuth(app)
-
-goolge = oauth.remote_app(
-    'google',
-    consumer_key='YOUR_CLIENT_ID',
-    consumer_secret='YOUR_CLIENT_SECRET',
-    request_token_params={
-        'scope': 'email'
-    },
-    base_url='https://www.googleapis.com/oauth2/v1/',
-    request_token_url=None,
-    access_token_method='POST',
-    access_token_url='https://accounts.google.com/o/oauth2/token',
-    authorize_url='https://accounts.google.com/o/oauth2/auth'
-)
-
 socketio.init_app(app, cors_allowed_origins="*")
-
 
 if __name__ == "__main__":
     socketio.run(app, debug=True, host='0.0.0.0', port=5000)
